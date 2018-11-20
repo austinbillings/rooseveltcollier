e3f4605f263e7de9638bf9669553c8510406fc36
austinbillings <austin@austinbillings.com>
2018-11-20 16:59:14 -0500

    some data updates

--- src/data/features.jsx
+++ src/data/features.jsx
@@ -5,6 +5,7 @@ const features = [
   {
     title: <span><b><i>Exit 16</i></b> Released</span>,
     image: {
+      position: '15% 50%',
       url: '/assets/exit-16.jpg'
     },
     accent: '#66754C',
@@ -23,7 +24,7 @@ const features = [
     image: {
       url: '/assets/photos/wall-distant.jpg'
     },
-    accent: '#DA5234',
+    accent: '#a25a4a',
     content: (
       <p>
         Roosevelt Collier is going on tour all over the southeastern U.S., with dates also in California and New Jersey, playing your favorites from his stunning debut, <b><i>Exit 16</i></b>. See where and when he's coming to a city near you.
@@ -35,8 +36,26 @@ const features = [
     }
   },
   {
+    title: <span>Let The Steel Play!</span>,
+    image: {
+      position: '50% 50%',
+      url: '/assets/albums/let-the-steel-play.jpg'
+    },
+    accent: 'rgb(68, 71, 133)',
+    content: (
+      <p>
+        Recorded late at night with a full moon shining through the windows in a Miami basement. For a limited time, get an exclusive free MP3 download of this live EP.
+      </p>
+    ),
+    action: {
+      text: <span>Check it out &nbsp; <Icon fa="angle-right"/></span>,
+      url: '/music#letthesteelplay'
+    }
+  },
+  {
     title: <span>Fresh tunes: <b style={{ whiteSpace: 'nowrap' }}><i>The Miami Sessions</i></b></span>,
     image: {
+      position: '50% 20%',
       url: '/assets/miami-sessions-alt.jpg'
     },
     accent: '#302F2F',
--- src/data/home-slides.jsx
+++ src/data/home-slides.jsx
@@ -27,12 +27,23 @@ const homeSlides = [
           on GroundUP Music!
           <img src="/assets/gu-logo.png" className="logo-small" />
         </big>
+        <p>
+          Produced by Snarky Puppy’s Michael League.
+        </p>
         <p className="white-text"><i>“This record is a record about me,” says Roosevelt Collier.<br/>“It’s telling a story of who I am, where I’m from, and where I’m going.”</i></p>
-        <a href="/music#exit16">
-          <button>
-            Listen Now &nbsp;<Icon fa="angle-right"/>
-          </button>
-        </a>
+        <div className="row wrap">
+          <a href="/music#exit16">
+            <button>
+              Listen Now &nbsp;<Icon fa="angle-right"/>
+            </button>
+          </a>
+          <span> &nbsp; </span>
+          <a href="/video">
+            <button>
+              Watch Now &nbsp;<Icon fa="angle-right"/>
+            </button>
+          </a>
+        </div>
       </div>
     )
   },
@@ -43,13 +54,20 @@ const homeSlides = [
     linkTo: '/music/bokante',
     accent: 'rgba(8, 72, 113, 1)',
     background: {
-      position: '30% 40%',
-      image: '/assets/photos/bokante-sessions.jpg'
+      position: '77% 40%',
+      image: '/assets/photos/bokante-group.jpg'
     },
     content: (
       <div>
         <h1>"The Dr." meets Creole.</h1>
-        <big>Check out <b>Bokanté</b>, the world music supergroup that's breaking down musical barriers.</big>
+
+        <p style={{ maxWidth: '500px' }}>
+          <i>File under</i>: world-music-blended polyrhythmic rocking soul.
+        </p>
+        <p style={{ maxWidth: '500px' }}>
+          Roosevelt is a founding member of <b>Bokante</b>, the world music supergroup that's breaking down musical barriers.
+        </p>
+
         <a href="http://www.bokante.com" target="_blank">
           <button>
             Check out Bokante &nbsp;<Icon fa="angle-right"/>
@@ -59,21 +77,28 @@ const homeSlides = [
     )
   },
   {
-    id: 'soul-of-miami',
-    title: `Soul Of Miami`,
-    linkTo: '/about',
+    id: 'let-the-steel-play',
+    title: `Let The Steel Play`,
+    linkTo: '/music#letthesteelplay',
     background: {
-      image: '/assets/photos/hd.jpg',
-      position: '43% 40%'
+      image: '/assets/photos/with-andy-hall.jpg',
+      position: '27% 40%'
     },
     accent: 'rgba(61, 61, 61, 0.8)',
     content: (
       <div>
-        <h1>SOUL OF MIAMI</h1>
-        <p style={{ maxWidth: '800px' }}>Read about why Roosevelt Collier has been Miami's premiere genre-defying blues steel guitarist for years— and how working with Snarky Puppy's Michael League has breathed life into an all-new solo project from <i>The Dr.</i> himself.</p>
-        <a href="/about">
+        <h1>Let The Steel play</h1>
+        <p style={{ maxWidth: '500px' }}>
+          <i>Sacred Steel</i> meets bluegrass in this exploration of the Slide Guitar featuring Roosevelt & the Infamous StringDusters&apos; own <b>Andy Hall</b>.
+        </p>
+        <a href="/music#letthesteelplay">
           <button>
-            Learn more <Icon fa="angle-right"/>
+            Listen now <Icon fa="angle-right"/>
+          </button>
+        </a>
+        <a href="/video#letthesteelplay">
+          <button>
+            Watch now <Icon fa="angle-right"/>
           </button>
         </a>
       </div>
