673f3aecff4fdff6d78333cee9de6b07bdc86ad8
austinbillings <austin@austinbillings.com>
2019-04-01 11:06:24 -0400

    better conditions for using prod server

--- src/data/config.js
+++ src/data/config.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = (window.location.host === 'rooseveltcollier.com')
+export const SERVER_URL = (window.location.host === 'rooseveltcollier.com' || window.location.host === 'www.rooseveltcollier.com' )
   ? 'https://rvc.internetcompany.io'
   : 'http://127.0.0.1:19362';
