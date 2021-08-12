ceb17d1e957796a52f20d889b2785497bd11e7c4
austinbillings <austin@austinbillings.com>
2021-08-11 22:04:03 -0400

    update root ref

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import 'xx';
 import 'font-awesome';
-import Root from 'views/root';
+import Root from './views/root';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
