3eb728e6edf17c1c2a24df57de0c8e409bad688c
austinbillings <austin@austinbillings.com>
2021-08-31 18:52:05 -0400

    tour tweaks

--- src/views/tour-page/tour-panel.jsx
+++ src/views/tour-page/tour-panel.jsx
@@ -32,7 +32,7 @@ class TourPanel extends React.Component {
     const { tourDates, loading, error } = this.state;
 
     return (
-      <div>
+      <div style={{ width: '100%' }}>
         {loading && <p>Loading. . .</p>}
         {tourDates && <EventList shortDates={shortDates} events={max ? tourDates.slice(0, max) : tourDates}/>}
         {error && <p>Sorry, there was an error loading the data. <pre><code>{JSON.stringify(error)}</code></pre></p>}
