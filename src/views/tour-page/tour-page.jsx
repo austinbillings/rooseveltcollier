f8b4788ff67e278901638e01802a778545d4426d
austin <austin@austinbillings.com>
2022-02-05 16:19:40 -0500

    tourpage: remove 2021 poster

--- src/views/tour-page/tour-page.jsx
+++ src/views/tour-page/tour-page.jsx
@@ -13,7 +13,6 @@ class TourPage extends React.Component {
     return (
       <grid className="tour-page">
         <stack style={{ width: '100%', flexGrow: 1 }}>
-          <img src="/assets/tours/fall-2021.jpg" style={{ margin: '30px auto', maxWidth: '90%', width: 500 }} />
           <SectionHeading align="center" text="On Tour"/>
           <TourPanel/>
         </stack>
